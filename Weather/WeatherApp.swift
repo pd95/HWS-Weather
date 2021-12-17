57c02e7affc028d1cf3bff0df57453f7830d508b
Philipp <pd95@users.noreply.github.com>
2021-12-17 19:25:03 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Philipp on 17.12.21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
