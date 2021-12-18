dd1bf2c3c794a217b93ffd4b9f89568d6bb6a1b6
Philipp <pd95@users.noreply.github.com>
2021-12-18 14:41:52 +0100

    Implemented "sky like background color" using a gradient, simulating day/night cycle - custom colors for midnight, sunrise, sunny day and sunset - top of gradient more bluish, bottom yellow/reddish during sunrise/sunset - reusable `Color.getComponents` and `Color.interpolated` methods

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -11,12 +11,37 @@ struct ContentView: View {
     @State private var cloudThickness = Cloud.Thickness.regular
     @State private var time = 0.0
 
+    let backgroundTopStops: [Gradient.Stop] = [
+        .init(color: .midnightStart, location: 0),
+        .init(color: .midnightStart, location: 0.25),
+        .init(color: .sunriseStart, location: 0.33),
+        .init(color: .sunnyDayStart, location: 0.38),
+        .init(color: .sunnyDayStart, location: 0.7),
+        .init(color: .sunsetStart, location: 0.78),
+        .init(color: .midnightStart, location: 0.82),
+        .init(color: .midnightStart, location: 1),
+    ]
+
+    let backgroundBottomStops: [Gradient.Stop] = [
+        .init(color: .midnightEnd, location: 0),
+        .init(color: .midnightEnd, location: 0.25),
+        .init(color: .sunriseEnd, location: 0.33),
+        .init(color: .sunnyDayEnd, location: 0.38),
+        .init(color: .sunnyDayEnd, location: 0.7),
+        .init(color: .sunsetEnd, location: 0.78),
+        .init(color: .midnightEnd, location: 0.82),
+        .init(color: .midnightEnd, location: 1),
+    ]
+
     var body: some View {
         ZStack {
             CloudsView(thickness: cloudThickness)
         }
         .preferredColorScheme(.dark)
-        .background(Color.blue)
+        .background(LinearGradient(colors: [
+            backgroundTopStops.interpolated(amount: time),
+            backgroundBottomStops.interpolated(amount: time)
+        ], startPoint: .top, endPoint: .bottom))
         .safeAreaInset(edge: .bottom, content: {
             VStack {
                 Text(formattedTime)
