01d89ceb6b264c821635fd8d376e692cb3bf64bc
Philipp <pd95@users.noreply.github.com>
2021-12-18 15:08:02 +0100

    Adding cloud tinting: similar to background gradient, but applied to the cloud images

--- Weather/CloudsView.swift
+++ Weather/CloudsView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CloudsView: View {
     var cloudGroup: CloudGroup
+    let topTint: Color
+    let bottomTint: Color
 
     var body: some View {
         TimelineView(.animation) { timeline in
@@ -16,13 +18,23 @@ struct CloudsView: View {
                 cloudGroup.update(date: timeline.date)
                 context.opacity = cloudGroup.opacity
 
+                let resolvedImages = (0..<8).map { i -> GraphicsContext.ResolvedImage in
+                    let sourceImage = Image("cloud\(i)")
+                    var resolved = context.resolve(sourceImage)
+
+                    resolved.shading = .linearGradient(
+                        Gradient(
+                            colors: [topTint, bottomTint]),
+                        startPoint: .zero,
+                        endPoint: CGPoint(x: 0, y: resolved.size.height)
+                    )
+                    return resolved
+                }
+
                 for cloud in cloudGroup.clouds {
                     context.translateBy(x: cloud.position.x, y: cloud.position.y)
                     context.scaleBy(x: cloud.scale, y: cloud.scale)
-
-                    let cloudImage = "cloud\(cloud.imageNumber)"
-                    context.draw(Image(cloudImage), at: .zero, anchor: .topLeading)
-
+                    context.draw(resolvedImages[cloud.imageNumber], at: .zero, anchor: .topLeading)
                     context.transform = .identity
                 }
             }
@@ -31,14 +43,16 @@ struct CloudsView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 
-    init(thickness: Cloud.Thickness) {
+    init(thickness: Cloud.Thickness, topTint: Color, bottomTint: Color) {
         cloudGroup = CloudGroup(thickness: thickness)
+        self.topTint = topTint
+        self.bottomTint = bottomTint
     }
 }
 
 struct CloudsView_Previews: PreviewProvider {
     static var previews: some View {
-        CloudsView(thickness: .regular)
+        CloudsView(thickness: .regular, topTint: .white, bottomTint: .white)
             .background(Color.blue)
     }
 }
