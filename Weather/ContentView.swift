4e9820c90f48b0b5251e610eefd2d527d83a8348
Philipp <pd95@users.noreply.github.com>
2021-12-18 12:48:37 +0100

    Adding a slider to simulate advancement of time

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var cloudThickness = Cloud.Thickness.regular
+    @State private var time = 0.0
 
     var body: some View {
         ZStack {
@@ -18,17 +19,32 @@ struct ContentView: View {
         .background(Color.blue)
         .safeAreaInset(edge: .bottom, content: {
             VStack {
+                Text(formattedTime)
+                    .padding(.top)
+
                 Picker("Thickness", selection: $cloudThickness) {
                     ForEach(Cloud.Thickness.allCases, id: \.self) { thickness in
                         Text(String(describing: thickness).capitalized)
                     }
                 }
                 .pickerStyle(.segmented)
+
+                HStack {
+                    Text("Time:")
+                    Slider(value: $time)
+                }
+                .padding()
             }
             .padding()
             .frame(maxWidth: .infinity)
             .background(.regularMaterial)
         })
+    }
+
+    var formattedTime: String {
+        let start = Calendar.current.startOfDay(for: Date.now)
+        let advanced = start.addingTimeInterval(time * 24 * 60 * 60)
+        return advanced.formatted(date: .omitted, time: .shortened)
     }
 }
 
